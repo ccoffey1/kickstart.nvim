3504c9fd312a63319bc7a533ccd31064d425f7e0
Christian Coffey <coffeychristian@gmail.com>
2025-02-02 16:40:41 -0500

    copilot!

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,4 +9,16 @@ return {
       vim.keymap.set({ 'n', 'v' }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
     end,
   },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      vim.api.nvim_set_keymap('i', '<C-j>', 'copilot#Accept("<CR>")', { expr = true, silent = true }),
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
