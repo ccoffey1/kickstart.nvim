bf0f71788bc411523b574292cdd0876189387363
Christian Coffey <coffeychristian@gmail.com>
2025-03-05 12:00:44 -0500

    New plugin to run commands in current directory. Probably a better way to do this but alas...A

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,9 +5,6 @@
 return {
   {
     'olrtg/nvim-emmet',
-    config = function()
-      vim.keymap.set({ 'n', 'v' }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
-    end,
   },
   {
     'github/copilot.vim',
