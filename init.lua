5b3b0dfd9fb5324822f506ae77cf3fdc32a471cb
Christian Coffey <coffeychristian@gmail.com>
2025-04-13 17:31:12 -0400

    new plugins

--- init.lua
+++ init.lua
@@ -708,9 +708,9 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd' },
-        typescript = { 'prettierd' },
-        java = { 'prettierd' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        java = { 'prettier' },
       },
     },
   },
---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,29 @@
+return {
+  -- Icons
+  { 'echasnovski/mini.nvim' },
+  { 'nvim-tree/nvim-web-devicons', opts = {} },
+
+  -- The actual plugin
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      explorer = { enabled = true, replace_netrw = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
+}
